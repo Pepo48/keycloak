a149506e83cced8704b9770ef243667e1ca16939
Robin Meese <39960884+robson90@users.noreply.github.com>
2026-04-07 09:15:38 +0200

    is-45364 migrate Client Api V2 tests to utilize adminClient
    Signed-off-by: Robin Meese <39960884+robson90@users.noreply.github.com>

--- rest/admin-v2/tests/src/test/java/org/keycloak/tests/admin/client/v2/AbstractClientApiV2Test.java
+++ rest/admin-v2/tests/src/test/java/org/keycloak/tests/admin/client/v2/AbstractClientApiV2Test.java
@@ -2,7 +2,10 @@ package org.keycloak.tests.admin.client.v2;
 
 import jakarta.ws.rs.core.HttpHeaders;
 
+import org.keycloak.admin.api.client.ClientApi;
+import org.keycloak.admin.api.client.ClientsApi;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.wrapper.Clients;
 import org.keycloak.services.client.ClientServiceHelper;
 import org.keycloak.testframework.annotations.InjectAdminClient;
 import org.keycloak.testframework.remote.runonserver.InjectRunOnServer;
@@ -28,8 +31,43 @@ public abstract class AbstractClientApiV2Test {
         return "http://localhost:8080/admin/api/%s/clients/v2".formatted(getRealmName());
     }
     public String getClientApiUrl(String clientId) {
-        return "http://localhost:8080/admin/api/%s/clients/v2/%s".formatted(getRealmName(), clientId);
+        return getClientApiUrl(getRealmName(), clientId);
     }
+
+    public String getClientApiUrl(String realmName, String clientId) {
+        return "http://localhost:8080/admin/api/%s/clients/v2/%s".formatted(realmName, clientId);
+    }
+
+    public ClientsApi getClientsApi() {
+        return adminClient.clients(getRealmName(), Clients.class).v2();
+    }
+
+    public ClientsApi getClientsApi(String realmName) {
+        return getClientsApi(adminClient, realmName);
+    }
+
+    public ClientsApi getClientsApi(Keycloak adminClient) {
+        return getClientsApi(adminClient, getRealmName());
+    }
+
+    public ClientsApi getClientsApi(Keycloak adminClient, String realmName) {
+        return adminClient.clients(realmName, Clients.class).v2();
+    }
+
+    public ClientApi getClientApi(String clientId) {
+        return getClientApi(getRealmName(), clientId);
+    }
+
+    public ClientApi getClientApi(String realmName, String clientId) {
+        return getClientApi(adminClient, realmName, clientId);
+    }
+
+    public ClientApi getClientApi(Keycloak adminClient, String realmName, String clientId) {
+        return adminClient.clients(realmName, Clients.class).v2().client(clientId);
+    }
+
+
+
 
     @BeforeAll
     public static void setupMapper() {
